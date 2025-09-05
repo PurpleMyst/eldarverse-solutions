0dab466673c9aa3c422216000b3684ee3c60325c
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-09-05 18:31:33 +0200

    perf(b): avoid branches and allocations

--- september-2025/problem-b/src/lib.rs
+++ september-2025/problem-b/src/lib.rs
@@ -10,27 +10,26 @@ pub fn solve() -> impl Display {
     let ks: Vec<[_; 10]> = (2..=6)
         .into_par_iter()
         .map(|n| {
+            let game_map: Vec<_> = (0..n)
+                .flat_map(|i| (0..n).map(move |j| (i, j)))
+                .filter(|(i, j)| i != j)
+                .map(|(i, j)| (i as usize, j as usize))
+                .collect();
+
             (0..1 << (n * (n - 1)))
                 .into_par_iter()
-                .map(|mut combo| {
-                    let mut scores = vec![0u8; n as usize];
-                    for i in 0..n {
-                        for j in 0..n {
-                            if i != j {
-                                if combo & 1 != 0 {
-                                    scores[i as usize] += 1;
-                                } else {
-                                    scores[j as usize] += 1;
-                                }
-                                combo >>= 1;
-                            }
-                        }
+                .map(|mut combo: u32| {
+                    let mut scores = [0; 6];
+                    for &(i, j) in &game_map {
+                        scores[i as usize] += combo & 1;
+                        scores[j as usize] += 1 - (combo & 1);
+                        combo >>= 1;
                     }
-                    let (min, max) = scores.into_iter().minmax().into_option().unwrap();
+                    let (min, max) = scores.into_iter().take(n).minmax().into_option().unwrap();
                     max - min
                 })
                 .fold(
-                    || [0; 10],
+                    || [0u32; 10],
                     |mut acc, delta| {
                         acc.iter_mut()
                             .take(delta as usize)
