f49d3170723794be411d4a86cc0b49e393907084
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-09-06 02:14:19 +0200

    chore: cargo fmt

--- september-2025/common/src/lib.rs
+++ september-2025/common/src/lib.rs
@@ -1,7 +1,13 @@
 use std::fmt::Display;
 
-pub fn cases<I>(it: I) -> String 
-where I: IntoIterator, I::Item: Display,
+pub fn cases<I>(it: I) -> String
+where
+    I: IntoIterator,
+    I::Item: Display,
 {
-    it.into_iter().enumerate().map(|(i, s)| format!("Case #{}: {}", i + 1, s)).collect::<Vec<_>>().join("\n")
+    it.into_iter()
+        .enumerate()
+        .map(|(i, s)| format!("Case #{}: {}", i + 1, s))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
--- september-2025/problem-a/src/lib.rs
+++ september-2025/problem-a/src/lib.rs
@@ -3,13 +3,12 @@ use std::fmt::Display;
 use common::cases;
 
 pub fn solve() -> impl Display {
-    cases(include_str!("input.txt").lines().skip(1)
-        .map(|line| {
-            let mut mask = 0u32;
-            for c in line.bytes() {
-                mask |= 1 << (c.to_ascii_lowercase() - b'a');
-            }
+    cases(include_str!("input.txt").lines().skip(1).map(|line| {
+        let mut mask = 0u32;
+        for c in line.bytes() {
+            mask |= 1 << (c.to_ascii_lowercase() - b'a');
+        }
 
-            100 - 5 * mask.count_ones()
-        }))
+        100 - 5 * mask.count_ones()
+    }))
 }
